7a406df7cbce3460bc5e3da8ae7764a97a8cde47
Elliott Williams <e@elliottwillia.ms>
2014-06-10 13:35:07 -0400

    Add user as owner and maintainer on module create

--- grails-app/controllers/org/openmrs/modulus/ModuleController.groovy
+++ grails-app/controllers/org/openmrs/modulus/ModuleController.groovy
@@ -1,18 +1,21 @@
 package org.openmrs.modulus
 
+import grails.plugin.springsecurity.annotation.Secured
 import grails.rest.RestfulController
-import grails.transaction.Transactional
 import org.codehaus.groovy.grails.web.servlet.HttpHeaders
 
 import static org.springframework.http.HttpStatus.CREATED
-import static org.springframework.http.HttpStatus.OK
 
 class ModuleController extends RestfulController {
     def moduleService
+    def springSecurityService
+
+    Class<Module> resource
 
     static responseFormats = ['json', 'xml']
     ModuleController() {
         super(Module)
+        resource = Module
     }
 
     /**
@@ -32,5 +35,76 @@ class ModuleController extends RestfulController {
 
     private def getBySlug(String slug) {
         Module.findWhere(slug: slug)
+    }
+
+    @Override
+    @Secured("ROLE_USER")
+    Object save() {
+        if(handleReadOnly()) {
+            return
+        }
+
+        def instance = new Module(params)
+
+        instance.validate()
+        if (instance.hasErrors()) {
+            respond instance.errors, view:'create' // STATUS CODE 422
+            return
+        }
+
+        // Add current user as the owner and maintainer
+        User current = springSecurityService.getCurrentUser()
+        instance.maintainers = [current]
+        instance.owner = current.username
+
+        instance.save flush:true
+
+        response.addHeader(HttpHeaders.LOCATION,
+                g.createLink(
+                        resource: this.controllerName, action: 'show',id: instance.id, absolute: true,
+                        namespace: hasProperty('namespace') ? this.namespace : null ))
+        respond instance, [status: CREATED]
+
+    }
+
+    @Override
+    @Secured("ROLE_USER")
+    Object update() {
+        def module = queryForResource(params.id)
+        if (unauthorized(module)) return
+
+        return super.update()
+    }
+
+    @Override
+    @Secured("ROLE_USER")
+    Object delete() {
+        def module = queryForResource(params.id)
+        if (!module) return notFound()
+        if (unauthorized(module)) return
+
+        return super.delete()
+    }
+
+    @Override
+    protected Object queryForResource(Serializable id) {
+        moduleService.get(id)
+    }
+
+    private hasPermissions(Module m) {
+        def current = springSecurityService.getCurrentUser()
+
+        def isMaintainer = moduleService.isMaintainer(m, current)
+        def isAdmin = current.hasRole("ROLE_ADMIN")
+
+        isMaintainer || isAdmin
+    }
+
+    private unauthorized(Module m) {
+        if (!hasPermissions(m)) {
+            render(status: 403, text: 'You are not authorized to access this resource'  )
+            return true
+        }
+        return false
     }
 }
--- test/unit/org/openmrs/modulus/ModuleControllerSpec.groovy
+++ test/unit/org/openmrs/modulus/ModuleControllerSpec.groovy
@@ -1,20 +1,41 @@
 package org.openmrs.modulus
 
+import grails.plugin.springsecurity.SpringSecurityService
+import grails.rest.RestfulController
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
+import spock.lang.Ignore
 import spock.lang.Specification
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(ModuleController)
+@Mock([Module, User, UserRole, Role, SpringSecurityService])
 class ModuleControllerSpec extends Specification {
 
     def setup() {
+        User.metaClass.encodePassword = { 'encoded_password '}
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
+    void "creating a module should add the current user as owner and maintainer in params"() {
+        given:
+        def testUser = new User(username: 'test', password: 'foo').save(failOnError: true)
+        UserRole.create(testUser, Role.findOrSaveByAuthority('ROLE_USER'))
+        RestfulController.metaClass.save = {}
+
+        def securityMock = mockFor(SpringSecurityService)
+        securityMock.demand.getCurrentUser() { -> testUser }
+        controller.springSecurityService = securityMock.createMock()
+
+        when:
+        controller.save()
+
+        then:
+        response.json.owner == testUser.username
+        response.json.maintainers[0].id == testUser.id
     }
 }
