e373187edf9e4fa3a3fc65e56630e8ebe8b86e43
Elliott Williams <e@elliottwillia.ms>
2014-06-10 12:09:12 -0400

    Wrote OmodParserService & tests

---
+++ test/unit/org/openmrs/modulus/OmodParserServiceSpec.groovy
@@ -0,0 +1,46 @@
+package org.openmrs.modulus
+
+import grails.test.mixin.TestMixin
+import grails.test.mixin.support.GrailsUnitTestMixin
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
+ */
+@TestMixin(GrailsUnitTestMixin)
+class OmodParserServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "getMetadata() should return properties of a valid omod"() {
+        given:
+        File omod = TestUtils.download('https://modules.openmrs.org/modulus/api/releases/1029/download/webservices.rest-2.5.e52eb0.omod')
+
+        when:
+        def data = service.getMetadata(omod)
+
+        then:
+        data.id == 'webservices.rest'
+        data.require_version == '1.8.1'
+        data.version == '2.5.e52eb0'
+        data.name == 'Rest Web Services'
+        data.description == 'Publishes Rest Web Services exposing the OpenMRS API'
+
+    }
+
+    void "ensureOmodFile() should throw a NonOmodError if file is not an omod"() {
+        given:
+        File nonOmod = TestUtils.download('http://upload.wikimedia.org/wikipedia/commons/a/af/Fred_Rogers.jpg')
+
+        when:
+        service.ensureOmodFile(nonOmod)
+
+        then:
+        thrown(NonOmodException)
+    }
+
+}
